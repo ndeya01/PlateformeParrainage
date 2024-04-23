43ee0a680352a3218393d68d823f09350a7d2344
ndeya01 <ndeyeawadieme01@gmail.com>
2024-04-23 14:25:54 +0000

    Finition

--- back-office/src/pages/Login.tsx
+++ back-office/src/pages/Login.tsx
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom'; // Importez useHistory pour gérer la redirection
+import axios from 'axios';
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleLogin = () => {
-        // Vérification des informations de connexion
-        if (email === "utilisateur" && password === "motdepasse") {
-            // Connexion réussie, rediriger vers la page appropriée
-            // Remplacez "/home" par le chemin de la page vers laquelle vous souhaitez rediriger
-            console.log("Connexion réussie !");
-        } else {
-            setError("Nom d'utilisateur ou mot de passe incorrect");
-        }
-    }
+    const navigate = useNavigate()
 
+    const handleLogin = () => {
+        axios.post('https://apex.oracle.com/pls/apex/wksp_parrainnageelection/plateforme_parrainage/authentification/', {
+            identifiant: email,
+            motDePasse: password
+        })
+        .then(response => {
+            console.log("Réponse de l'API:", response.data);
+            if (response.status === 200) {
+                navigate('/'); // Rediriger si la connexion est réussie
+            } else {
+                setError("Nom d'utilisateur ou mot de passe incorrect");
+            }
+        })
+        .catch(error => {
+            console.error("Erreur lors de la connexion:", error);
+            setError("Une erreur s'est produite lors de la connexion.");
+        });
+    }
     return (
         <div className="flex justify-center items-center h-screen bg-gray-800">
             <div className="w-1/3 p-6 border rounded-lg shadow-lg bg-white">
@@ -30,9 +40,7 @@ const Login = () => {
                     <label htmlFor="password" className="block mb-1">Mot de passe</label>
                     <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full border px-2 py-1" />
                 </div>
-                <Link to="/">
-                    <button onClick={handleLogin} className="bg-gray-900 text-white px-4 py-2 rounded hover:bg-blue-600">Se connecter</button>
-                </Link>
+                <button onClick={handleLogin} className="bg-gray-900 text-white px-4 py-2 rounded hover:bg-blue-600">Se connecter</button>
             </div>
         </div>
     );
